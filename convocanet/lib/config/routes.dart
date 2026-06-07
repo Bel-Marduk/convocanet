7fbd9946d15a017675f197cbaae155d2dff3dc89
ConvocaNet <user@convocanet.org>
2026-06-07 07:22:52 -0600

    fix(routes): import supabase_flutter for AuthState type used in debug helper

--- convocanet/lib/config/routes.dart
+++ convocanet/lib/config/routes.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart';
 import 'package:go_router/go_router.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 import '../screens/landing/landing_screen.dart';
 import '../screens/auth/login_screen.dart';
